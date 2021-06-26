22d0cee8b235df0837718fbdadd3d3b2fa356b71
Chase Voorhees <chase@csvsoftware.com>
2021-06-25 21:50:16 -0700

    actual changes...

--- cbapi_cron_day.sh
+++ cbapi_cron_day.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
+SERVICE='cbapiservicecon'
 
-# Used in production to restart service if it crashes (not that I've seen that issue) or is leaking mem (nor this)
-
-SERVICE='abapiservicecon'
- 
 if ps ax | grep -v grep | grep $SERVICE > /dev/null
 then
     echo "$SERVICE service running, everything is fine"
 else
     echo "$SERVICE is not running, starting"
-    sudo /home/ubuntu/Release_Ubu64/cbapiserviceconsole
+    sudo nohup /home/ubuntu/Release_Ubu64/cbapiserviceconsole &
 fi
--- cbapi_cron_week.sh
+++ cbapi_cron_week.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-
-# Used in production to restart service to ensure pem keys used are up to date
-
 SERVICE='cbapiserviceconsole'
- 
+
 sudo pkill cbapiservicecon
 sudo pkill cbapiserviceconsole
-sudo /home/ubuntu/Release_Ubu64/cbapiserviceconsole
+sudo nohup /home/ubuntu/Release_Ubu64/cbapiserviceconsole &
+
